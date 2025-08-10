e2aae91ee8d0140dd45e3b3f71ab28f94d4d1c7d
Pallavi Raj <pallaviraj985@gmail.com>
2025-08-10 21:57:30 +0530

    Remove .vs folder from version control. minor layout tweaks for exploration

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,40 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent) //Color.fromRGBD(0,0,125,1) four parameters
         ),
         home: MyHomePage(),
       ),
     );
   }
 }
-//changenotifier is kind a like anyone who is watching me or listening to me, do take a note
-//or maybe update yourself. --It's really cool.
+//changenotifier is kinda like "Oh! I have changed. Anyone who is watching me or listening to me, should take a note
+//or maybe update yourself." --It's really cool.
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random cool ideas: '),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
           ElevatedButton(
             onPressed: () {
-              debugPrint('button pressed!');
+              //debugPrint('button pressed!');
+              appState.getNext();
             }, 
             child: Text('Next'),
           ),
@@ -52,3 +59,30 @@ class MyHomePage extends StatelessWidget{
     );
   }
 }
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  }); 
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(pair.asLowerCase, style: style,),
+      ),
+    );
+  }
+}
+
+//color actually animated : Implicit animation feature of flutter
